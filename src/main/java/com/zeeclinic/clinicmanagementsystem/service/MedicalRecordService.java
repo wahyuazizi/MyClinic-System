056a6c9aee82592e89ec50b41bcae19e2bcea872
Wahyu Azizi <wahyuazizi03@gmail.com>
2026-03-08 05:06:23 +0800

    update: all methods in medical record service impl

--- src/main/java/com/zeeclinic/clinicmanagementsystem/service/MedicalRecordService.java
+++ src/main/java/com/zeeclinic/clinicmanagementsystem/service/MedicalRecordService.java
@@ -4,10 +4,11 @@ import com.zeeclinic.clinicmanagementsystem.model.dto.request.MedicalRecordReque
 import com.zeeclinic.clinicmanagementsystem.model.dto.response.MedicalRecordResponse;
 import com.zeeclinic.clinicmanagementsystem.model.entity.MedicalRecord;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface MedicalRecordService extends CrudService<MedicalRecordRequest, MedicalRecordResponse> {
 
-    MedicalRecord findByPatientId(UUID id);
+    List<MedicalRecordResponse> findByPatientId(UUID id);
 
 }
