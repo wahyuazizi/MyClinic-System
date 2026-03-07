2690d7bd599a90acdda8694440938592223d90e5
Wahyu Azizi <wahyuazizi03@gmail.com>
2026-03-07 21:12:51 +0800

    update: patient dto req using regular class

--- src/main/java/com/zeeclinic/clinicmanagementsystem/model/dto/request/PatientRequest.java
+++ src/main/java/com/zeeclinic/clinicmanagementsystem/model/dto/request/PatientRequest.java
@@ -1,11 +1,40 @@
 package com.zeeclinic.clinicmanagementsystem.model.dto.request;
 
+import com.zeeclinic.clinicmanagementsystem.model.enums.Gender;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class PatientRequest {
+
+    @NotNull(message = "id is required")
+    private UUID id;
+
+    @NotBlank(message = "full name is required")
+    private String fullName;
+
+    @NotBlank(message = "nik is required")
+    private String nik;
+
+    @NotNull(message = "birth date is required")
+    private LocalDate birthDate;
+
+    @NotBlank(message = "phone number is required")
+    private String phoneNumber;
+
+    private Gender gender;
+
+    private String email;
+
+    private String address;
+
+    private String bloodType;
 }
